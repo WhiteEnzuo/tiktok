3a6c0bc7f19567b4684913838fb09a966881d896
enzuo <210327422@qq.com>
2023-02-17 23:14:01 +0800

    上传sql

--- VideoService/sql/sql.sql
+++ VideoService/sql/sql.sql
@@ -28,7 +28,7 @@ CREATE TABLE `file`
 (
     `md5`       varchar(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  NOT NULL COMMENT '视频唯一标志',
     `video_url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '视频url',
-    `fileName`  varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '文件名字',
+    `file_name`  varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '文件名字',
     `image_url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '图片url',
     PRIMARY KEY (`md5`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
